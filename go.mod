e1cddfb40c48bcce3bd8f60283333c11d4bd3adc
Milad <mindwingx@gmail.com>
2023-08-24 15:32:59 +0200

    [branch][master] Init

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/mindwingx/go-cache-wrapper
+
+go 1.19
+
+require (
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/mindwingx/abstraction v0.0.0-20230824122946-df8f45519b62
+	github.com/mindwingx/go-helper v0.0.0-20230823115142-6448921aaddd
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	gorm.io/gorm v1.25.4 // indirect
+)
---
+++ wrapper.go
@@ -0,0 +1,85 @@
+package cachewrapper
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	redisCore "github.com/go-redis/redis/v8"
+	"github.com/mindwingx/abstraction"
+	"github.com/mindwingx/go-helper"
+	"time"
+)
+
+type (
+	redis struct {
+		config cacheConfig
+		locale abstraction.Locale
+		redis  *redisCore.Client
+	}
+
+	cacheConfig struct {
+		Host     string
+		Port     string
+		Password string
+		Db       int
+		Timeout  time.Duration
+	}
+)
+
+func NewRedis(registry abstraction.Registry, locale abstraction.Locale) abstraction.Cache {
+	caching := new(redis)
+	err := registry.Parse(&caching.config)
+	if err != nil {
+		helper.CustomPanic("", err)
+	}
+	caching.locale = locale
+
+	caching.redis = redisCore.NewClient(&redisCore.Options{
+		Addr:     fmt.Sprintf("%s:%s", caching.config.Host, caching.config.Port),
+		Password: caching.config.Password,
+		DB:       caching.config.Db,
+	})
+
+	return caching
+}
+
+func (r *redis) InitCache() {
+	ctx, cancel := context.WithTimeout(context.Background(), r.config.Timeout)
+	// call the cancel variable of the above context
+	defer cancel()
+
+	_, errConnect := r.redis.Ping(ctx).Result()
+	if errConnect != nil {
+		helper.CustomPanic(r.locale.Get("cache_conn_err"), errConnect)
+	}
+}
+
+func (r *redis) Store(key string, data interface{}, duration time.Duration) (err error) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if err = r.redis.Set(context.Background(), key, bytes, duration).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *redis) Exists(key string) bool {
+	return r.redis.Exists(context.Background(), key).Val() == 1
+}
+
+func (r *redis) Get(key string) (b []byte, err error) {
+	b, err = r.redis.Get(context.Background(), key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func (r *redis) Delete(key string) error {
+	return r.redis.Del(context.Background(), key).Err()
+}
